43ce06dd53b81121e71f83c4f5cb63bd9f25e8d9
rosetintedcheeks <austin.oaks02@gmail.com>
2022-05-06 08:18:58 -0400

    Added twopass support and other minor changes

--- main.py
+++ main.py
@@ -7,21 +7,48 @@ Spilts a files into segments and encodes those segments seperately using ffmpeg.
 '''
 inputFile = os.path.basename(sys.argv[len(sys.argv) - 1])
 args = ["ffmpeg", "-i", inputFile]
-encodeArgs = [
-    "-y",
-    "-c:v", "hevc",
-    "-crf:v", "23",
-    "-pix_fmt", "yuv420p10le",
-    "-preset:v", "slower",
-    "-c:a", "copy", 
-    "-map", "0:v",
-    "-map", "0:a",
-    "-f", "matroska"
-]
-segmentLocation = os.path.expanduser("~/fast/sugment/")
-markerFile = os.path.expanduser("~/fast/resume.txt")
-newSegmentLocation = os.path.expanduser("~/fast/new-sugment/")
-os.chdir(os.path.expanduser("~/fast"))
+twoPass = True
+if not twoPass:
+    encodeArgs = [
+        "-y",
+        "-c:v", "hevc",
+        "-crf:v", "23",
+        "-pix_fmt", "yuv420p10le",
+        "-preset:v", "slower",
+        "-c:a", "copy", 
+        "-map", "0:v",
+        "-map", "0:a",
+        "-f", "matroska"
+    ]
+else:
+    firstEncodeArgs = [
+        "-y",
+        "-c:v", "libvpx-vp9",
+        "-crf:v", "30",
+        "-b:v", "0",
+        "-pass", "1",
+        "-an",
+        "-pix_fmt", "yuv420p10le",
+        "-f", "null",
+    ]
+    secondEncodeArgs = [
+        "-y",
+        "-c:v", "libvpx-vp9",
+        "-crf:v", "30",
+        "-b:v", "0",
+        "-row-mt", "1",
+        "-tiles", "4x4",
+        "-pass", "2",
+        "-pix_fmt", "yuv420p10le",
+        "-c:a", "libopus", 
+        "-map", "0:v",
+        "-map", "0:a",
+        "-f", "webm"
+    ]
+segmentLocation = os.path.expanduser("~/share/sugment/")
+markerFile = os.path.expanduser("~/share/resume.txt")
+newSegmentLocation = os.path.expanduser("~/share/new-sugment/")
+os.chdir(os.path.expanduser("~/share"))
 
 def ffmpegSegment(args, inputFile, segmentLocation):
     print("Segmenting file...")
@@ -43,38 +70,44 @@ def ffmpegSegment(args, inputFile, segmentLocation):
     #TODO: check for errors
     subprocess.run(args)
 
-def ffmpegEncodeSegments(args, encodeArgs, inputFile, segmentLocation, marker):
-    print("Encoding segments...")
-    markerLocation = readMarker(marker)
+def ffmpegEncodeSegments(segment, args, encodeArgs, inputFile, segmentLocation, marker, outputFile=None, outputExtension="mkv"):
     totalSegments = countSegments(segmentLocation)
-    for segment in range(markerLocation, totalSegments):
-        writeMarker(marker, segment)
-        segmentArgs = args.copy()
-        completedPercentage = (segment / totalSegments) * 100
-        print("Encoding segment #" + f"{segment:04d}" + " out of " + f"{totalSegments:04d}" + " (" + f"{completedPercentage:.2f}" + "%)")
-        for a in encodeArgs:
-            segmentArgs.append(a)
+    writeMarker(marker, segment)
+    segmentArgs = args.copy()
+    completedPercentage = (segment / totalSegments) * 100
+    if outputFile is not None:
+        print("Encoding segment #" + f"{segment+1:04d}" + " out of " + f"{totalSegments:04d}" + " (" + f"{completedPercentage:.2f}" + "%)")
+    for a in encodeArgs:
+        segmentArgs.append(a)
 
-        # result will be file.ext => file.0000.ext
-        filePath = inputFile.split(".")
-        filePath.insert(len(filePath) - 1, f"{segment:04d}")
-        segmentInputFile = '.'.join(filePath)
+    # result will be file.ext => file.0000.ext
+    filePath = inputFile.split(".")
+    filePath.insert(len(filePath) - 1, f"{segment:04d}")
+    segmentInputFile = '.'.join(filePath)
 
-        # changing the input file to the selected segment
-        segmentArgs[2] = segmentLocation + segmentInputFile
+    # changing the input file to the selected segment
+    segmentArgs[2] = segmentLocation + segmentInputFile
 
-        # use mkv ext on output
-        filePath[len(filePath) - 1] = "mkv"
-        segmentOutputFile = '.'.join(filePath)
+    if outputFile is None:
+        # change output file's extension to outputExtension (default mkv)
+        filePath[len(filePath) - 1] = outputExtension
+        segmentOutputFile = newSegmentLocation + '.'.join(filePath)
+    else:
+        segmentOutputFile = outputFile
 
-        segmentArgs.append(newSegmentLocation + segmentOutputFile)
 
-        try:
-            subprocess.run(segmentArgs, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError:
-            exit("ffmpeg call failed while encoding segments")
+    segmentArgs.append(segmentOutputFile)
 
-def ffmpegReconnectSegments(args, inputFile, newSegmentLocation):
+    try:
+        subprocess.run(segmentArgs, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        exit("ffmpeg call failed while encoding segments")
+
+def ffmpegTwoPassEncodeSegments(segment, args, encodeArgs, inputFile, segmentLocation, marker):
+    ffmpegEncodeSegments(segment, args.copy(), encodeArgs[0], inputFile, segmentLocation, marker, outputFile="/dev/null")
+    ffmpegEncodeSegments(segment, args.copy(), encodeArgs[1], inputFile, segmentLocation, marker, outputExtension="webm")
+
+def ffmpegReconnectSegments(args, inputFile, segmentLocation, newSegmentLocation):
     reconnectFileName = newSegmentLocation + "reconnect.ffconcat"
     with open(reconnectFileName, "w") as reconnectFile:
         nslList = os.listdir(newSegmentLocation)
@@ -82,20 +115,30 @@ def ffmpegReconnectSegments(args, inputFile, newSegmentLocation):
         for file in nslList:
             if file == "reconnect.ffconcat":
                 continue
-            reconnectFile.write("file " + file + "\n")
+            reconnectFile.write("file " + 
+            file
+            .replace("'", "\\'") # escape '
+            .replace(" ", "\\ ") # escape space
+             + "\n")
     outputFile = inputFile.split(".")
     outputFile[len(outputFile) - 1] = "mkv"
     outputFile = '.'.join(outputFile)
     tmpArgs = [
         args[0],
         "-f", "concat",
+        "-safe", "0",
         "-i", reconnectFileName,
         "-c", "copy",
         "-fflags", "+genpts",
         "-f", "matroska",
         outputFile
     ]
+    for file in os.listdir(segmentLocation):
+        os.remove(os.path.abspath(segmentLocation + file))
     subprocess.run(tmpArgs)
+    #scary
+    #for file in os.listdir(newSegmentLocation):
+    #    os.remove(os.path.abspath(newSegmentLocation + file))
 
 def countSegments(segmentLocation):
     return len(os.listdir(segmentLocation))
@@ -125,9 +168,15 @@ if __name__ == "__main__":
                 for file in os.listdir(newSegmentLocation):
                     os.remove(os.path.abspath(newSegmentLocation + file))
         # segmenting
-        ffmpegSegment(args.copy(), inputFile, segmentLocation, newSegmentLocation)
+        ffmpegSegment(args.copy(), inputFile, segmentLocation)
         writeMarker(marker, 0)
 
-    ffmpegEncodeSegments(args.copy(), encodeArgs.copy(), inputFile, segmentLocation, marker)
-    ffmpegReconnectSegments(args.copy(), inputFile, newSegmentLocation)
+    print("Encoding segments...")
+    for segment in range(readMarker(marker), countSegments(segmentLocation)):
+        if twoPass:
+            encodeArgs = [firstEncodeArgs.copy(), secondEncodeArgs.copy()]
+            ffmpegTwoPassEncodeSegments(segment, args.copy(), encodeArgs, inputFile, segmentLocation, marker)
+        else:
+            ffmpegEncodeSegments(segment, args.copy(), encodeArgs.copy(), inputFile, segmentLocation, marker)
+    ffmpegReconnectSegments(args.copy(), inputFile, segmentLocation, newSegmentLocation)
     marker.close()
